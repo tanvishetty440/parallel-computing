83a8c7c89ec7f98d821ca1b5258e51e26e88ad1e
tanvishetty440 <tanvishetty440@gmail.com>
2025-08-18 10:08:16 +0530

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <omp.h>
+
+int fib(int n) {
+    int i, j;
+    if (n < 2)
+        return n;
+    else {
+        #pragma omp task shared(i) firstprivate(n)
+        i = fib(n - 1);
+
+        #pragma omp task shared(j) firstprivate(n)
+        j = fib(n - 2);
+
+        #pragma omp taskwait
+        return i + j;
+    }
+}
+
+int main() {
+    int n;
+    double start_time, end_time;
+
+    printf("Enter the Fibonacci number to calculate: ");
+    scanf("%d", &n);
+
+    omp_set_dynamic(0);
+    omp_set_num_threads(4);
+
+    start_time = omp_get_wtime(); 
+
+    #pragma omp parallel shared(n)
+    {
+        #pragma omp single
+        {
+            int result = fib(n);
+            end_time = omp_get_wtime(); 
+            printf("fib(%d) = %d\n", n, result);
+            printf("Time taken: %f seconds\n", end_time - start_time);
+        }
+    }
+
+    return 0;
+}
+
